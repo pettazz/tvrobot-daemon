cc5e972a38bc7601eca5df7887d0fd062ca5af70
Nick Pettazzoni <pettazz@gmail.com>
2014-10-28 09:46:10 -0400

    well this is broken

--- core/models/base_model.py
+++ core/models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
     @classmethod
     def findOne(cls, criteria = {}):
         logr = Logger.get_logger(__name__)
-        # row = Database().get_one('SELECT * FROM %s WHERE ')
+        row = Database().get_one('SELECT * FROM %s WHERE ')
 
     @classmethod
     def findAll(cls, criteria = {}):
@@ -27,6 +27,26 @@ class BaseModel:
     def create(cls, self):
         logr = Logger.get_logger(__name__)
         pass
+
+    @classmethod
+    def parse_criteria(cls, criteria):
+        result = ''
+        for key in criteria:
+            if key.upper() == 'NOT':
+                result += ' NOT ( %s )' % cls.parse_criteria(criteria[key])
+            elif key.upper() in ['AND', 'OR']:
+                result += '( '
+                #parse each value in criteria[key] and stick key in between them
+                innerResults = []
+                for innerKey in criteria[key]:
+                    innerResults.append(cls.parse_criteria({innerKey: criteria[key[innerKey]]}))
+                result += innerResults.join(key.upper())
+                result +=' ) '
+            else:
+                result += '%s = %s' % (key, criteria[key])
+
+        return result
+
 
 
     def __init__(self, data = None):
